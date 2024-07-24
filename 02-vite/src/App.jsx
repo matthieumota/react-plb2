4dd77e09ef62e474c80f6883c4e002a978a546d1
Matthieu Mota <matthieumota@gmail.com>
2024-07-24 16:21:36 +0200

    Vite  + State

---
+++ 02-vite/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <h1>React + Vite</h1>
+      <Button emoji="🐈‍⬛">Envoyer</Button>
+      <Button emoji="🐆">Valider</Button>
+      <Button>Confirmer</Button>
+    </div>
+  )
+}
+
+export default App
